2755aeef1b3bad8922feebb15f434dafb9f9c392
cybuild <macn51@proton.me>
2026-01-30 12:34:50 -0500

    Add a Main Content Page in Main.js
    This page introduces the front-end designer

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,18 @@ import './styles/Main.scss';
 
 const Main =() => {
     return (
-        <div>
+        <div className='main'>
+            <div className="main__container">
+                <div className="main__content">
+                    <div className="text">
+                        <p>Hello Everyone!</p>
+                        <h1>
+                            I am Cybuildguild
+                        </h1>
+                        <p>Front-end Developer & Designer</p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
