9ec017eb4eee22f5d7d07d320bbb2ad868d09b8d
cybuild <macn51@proton.me>
2026-01-30 19:30:52 -0500

    Update App.js to Include Contact.js and Section.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import './App.scss';
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Contact from "./components/Contact";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Section/>
+      <Contact/>
     </div>
   );
 }
